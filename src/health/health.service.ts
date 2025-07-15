96f3296196fbb208b263bbc5198a9d39f86ae2ac
Matt Sergeant <helpme@gmail.com>
2025-07-15 09:22:04 -0400

    Fix disk usage stuff

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -7,7 +7,7 @@ import { ConfigService } from '@nestjs/config/dist/config.service';
 @Injectable()
 export class HealthService {
   constructor(
-    private readonly configService: ConfigService<{ REDIS_MEMORY_THRESHOLD: number }>,
+    private readonly configService: ConfigService<{ REDIS_MEMORY_THRESHOLD_BYTES: number; DISK_THRESHOLD_PERCENT: number }>,
     private readonly health: HealthCheckService,
     private readonly disk: DiskHealthIndicator,
     private readonly prisma: PrismaService, // Assuming PrismaService is imported from the correct path
@@ -17,9 +17,10 @@ export class HealthService {
   ) {}
 
   async checkHealth(): Promise<HealthCheckResult> {
-    const redisMemoryThreshold = this.configService.get<number>('REDIS_MEMORY_THRESHOLD', 1000000000, { infer: true }); // Default to 1GB if not set
+    const redisMemoryThreshold = this.configService.get<number>('REDIS_MEMORY_THRESHOLD_BYTES', 1000000000, { infer: true }); // Default to 1GB if not set
+    const diskThreshold = this.configService.get<number>('DISK_THRESHOLD_PERCENT', 0.75, { infer: true }); // Default to 75% if not set
     return this.health.check([
-      () => this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+      () => this.disk.checkStorage('storage', { path: '/', thresholdPercent: diskThreshold }),
       () => this.prismaHealth.pingCheck('prisma', this.prisma),
       async () => this.redisHealth.isHealthy('redis', { client: await this.redisService.getClient(), memoryThreshold: redisMemoryThreshold }),
     ]);
